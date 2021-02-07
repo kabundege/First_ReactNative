455d459f83ec90bd0384c979669373ded9f2c25b
kabundege <christophekwizera1@gmail.com>
2021-02-07 15:57:51 +0200

    outputing lists

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
 import React,{ Component } from 'react';
-import img from './assets/splash.png'
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import img from './assets/splash.png';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      text:"kabundege",
-      holder:""
+      data:[
+        {name:'ange',age:1},
+        {name:'ange',age:2},
+        {name:'ange',age:3},
+        {name:'ange',age:4},
+        {name:'ange',age:5},
+        {name:'ange',age:6},
+        {name:'ange',age:7},
+        {name:'ange',age:8},
+      ]
     }
   }
   render(){
-    const { text } = this.state;
+    const { data } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>My Name is {text}</Text>
-        <TextInput style={styles.input} placeholder=" New Name ...." onChangeText={(value)=>this.setState({ holder: value })}/>
-        <View style={styles.button}>
-            <Button title="Update Name" onPress={()=>this.setState({ text: this.state.holder })}/>
-        </View>
+        <FlatList
+          keyExtractor={(item)=> data.indexOf(item)}
+          data={data}
+          numColumns={2}
+          renderItem={({ item })=>
+              <Text style={styles.person}>{item.name} is {item.age} old</Text>
+            }
+        />
+        {/* <ScrollView >
+          {data.map((item,index)=>
+          <View key={index} style={styles.person}>
+            <Text>{item.name} is {item.age} old</Text>
+          </View>
+          )}
+        </ScrollView> */}
       </View>
     );
   }
@@ -28,8 +46,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   text:{
     fontWeight:'bold',
@@ -45,5 +63,10 @@ const styles = StyleSheet.create({
     marginTop:20,
     padding:10,
     paddingVertical:5
+  },
+  person:{
+    padding:20,
+    margin:20,
+    backgroundColor:'dodgerblue'
   }
 });
