fa3a6238c64289fcc5001d4ebe18edaa0c80cf44
kabundege <christophekwizera1@gmail.com>
2021-02-08 10:50:46 +0200

    finalizing the todoApp (CRUD Ops)

--- App.js
+++ App.js
@@ -1,24 +1,57 @@
 import React,{ Component } from 'react';
 import img from './assets/splash.png'
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View,  } from 'react-native';
 
 export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      text:"kabundege",
-      holder:""
+      content:[
+        {name:'kabundege',age:21},
+        {name:'kabundege',age:24},
+        {name:'kabundege',age:27},
+        {name:'kabundege',age:30},
+      ],
+      input:''
     }
   }
   render(){
-    const { text } = this.state;
+    const { content } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>My Name is {text}</Text>
-        <TextInput style={styles.input} placeholder=" New Name ...." onChangeText={(value)=>this.setState({ holder: value })}/>
-        <View style={styles.button}>
-            <Button title="Update Name" onPress={()=>this.setState({ text: this.state.holder })}/>
-        </View>
+          <Text style={styles.header}>My Todos</Text>
+          <View>
+            <FlatList
+              keyExtractor={(item) => content.indexOf(item)}
+              data={content}
+              renderItem={({ item })=>
+                  <Text 
+                    style={styles.person}
+                    onPress={()=> this.setState((prevState,props)=>({ 
+                      content : prevState.content.filter(el=> el.age != item.age) 
+                    })
+                    )}
+                    >
+                      {item.name} is {item.age} old
+                  </Text>
+                }
+            />
+          </View>
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="new task"
+              placeholderTextColor="whitesmoke"
+              onChangeText={(val)=>this.setState({ input :  val})}
+            />
+            <Button
+              title="Add"
+              color="coral"
+              onPress={(e)=> 
+                this.setState({ content : [{ name:input,age:30 },...this.state.content]})
+              }
+            />
+          </View>
       </View>
     );
   }
@@ -27,23 +60,35 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
   },
-  text:{
+  header:{
+    padding:20,
+    backgroundColor:"coral",
+    fontSize:30,
     fontWeight:'bold',
-    color:'crimson'
+    textAlign:"center",
+    color:'whitesmoke',
+    marginBottom:30
   },
-  button:{
-    marginTop:20,
+  person:{
+    padding:15,
+    margin:5,
+    color:"white",
+    borderRadius:5,
+    borderWidth:1,
+    borderColor:"lightgrey",
+    borderStyle:"dashed"
   },
   input:{
+    padding:20,
+    backgroundColor:'#333',
+    margin:10,
+    color:'white',
+    borderRadius:50,
     borderWidth:1,
     borderColor:"grey",
-    borderRadius:10,
-    marginTop:20,
-    padding:10,
-    paddingVertical:5
+    shadowColor:"#333",
+    shadowRadius:10,
   }
 });
